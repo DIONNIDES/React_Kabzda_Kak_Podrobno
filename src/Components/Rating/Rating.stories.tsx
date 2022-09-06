73019fea4e9d1ee979d2b42b98c4e03154034c4a
DIONNIDES <dimakurgan123789@gmail.com>
2022-09-06 12:36:16 +0300

    storybook, add SelfControlled components

--- src/Components/Rating/Rating.stories.tsx
+++ src/Components/Rating/Rating.stories.tsx
@@ -7,14 +7,14 @@ export default {
     component: Rating,
 };
 
-export const EmptyRating = () => <Rating value={0} setRatingValue={(x) => x}/>
-export const Rating1 = () => <Rating value={1} setRatingValue={(x) => x}/>
-export const Rating2 = () => <Rating value={2} setRatingValue={(x) => x}/>
-export const Rating3 = () => <Rating value={3} setRatingValue={(x) => x}/>
-export const Rating4 = () => <Rating value={4} setRatingValue={(x) => x}/>
-export const Rating5 = () => <Rating value={5} setRatingValue={(x) => x}/>
+export const EmptyRating = () => <Rating value={0} evaluate={(x) => x}/>
+export const Rating1 = () => <Rating value={1} evaluate={(x) => x}/>
+export const Rating2 = () => <Rating value={2} evaluate={(x) => x}/>
+export const Rating3 = () => <Rating value={3} evaluate={(x) => x}/>
+export const Rating4 = () => <Rating value={4} evaluate={(x) => x}/>
+export const Rating5 = () => <Rating value={5} evaluate={(x) => x}/>
 
 export const RatingChanging = () => {
     const [ratingValue, setRatingValue] = useState<number>(0);
-    return <Rating value={ratingValue} setRatingValue={setRatingValue}/>
+    return <Rating value={ratingValue} evaluate={setRatingValue}/>
 }
